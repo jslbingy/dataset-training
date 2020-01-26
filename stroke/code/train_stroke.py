d57146fc09fb42fc585b5253d257e3c9a29423fa
PAN LUO <panluo@PANs-MacBook-Pro.local>
2020-01-26 21:30:21 +0800

    add

--- stroke/code/train_stroke.py
+++ stroke/code/train_stroke.py
@@ -14,8 +14,23 @@ test_data = test_data.drop(['id'], axis=1)
 
 # %%
 # do some data analysis before training and testing data
+stroke_data = train_data[train_data['stroke'] == 1]
 # stroke vs gender
-count_female_stroke = pd.DataFrame(train_data, gender == 1 & stroke == 1)
-print(count_female_stroke)
+count_female_stroke = len(stroke_data[stroke_data['gender'] == '0'])
+count_male_stroke = len(stroke_data[stroke_data['gender'] == '1'])
+
+# stroke vs age
+count_age_stroke_map = {}
+for i in range(10):
+    min_age = i*10
+    max_age = (i+1)*10
+    count_age_stroke_map[str(i)] = len(
+        stroke_data[(stroke_data['age'] >= min_age) & (stroke_data['age'] < max_age)])
 
 # %%
+# stroke vs
+count_hypertension_stroke = len(
+    stroke_data[stroke_data['hypertension'] == '1'])
+print(count_hypertension_stroke)
+
+ß  # %%
